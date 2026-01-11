7336e2dec083353df55c3edc3a1bb8912d164a2e
Dewnuwan Jayaweera <dewnuwan.jayaweera7@gmail.com>
2026-01-11 09:33:45 +0530

    Simple Mortgage Calculator

---
+++ 01_Mortagage_Calculator.java
@@ -0,0 +1,32 @@
+import  java.util.*;
+import java.text.NumberFormat;
+
+public class TM_FILENAME_BASE {
+  public static void main(){
+    int principal;
+    float rate;
+    final byte percent = 100;
+    byte amountOfYears = 0;
+    final byte amountOfMonths = 12;
+    NumberFormat currency = NumberFormat.getCurrencyInstance();    
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Principal: ");
+    principal = scanner.nextInt();
+
+    System.out.print("Annutal Rate: ");
+    rate = scanner.nextFloat()/amountOfMonths/percent;
+    
+
+    System.out.print("Time Period: ");
+    amountOfYears = scanner.nextByte();
+    
+    int numberOfPayments = amountOfYears * amountOfMonths;
+    
+    double monthlyPayment = principal * (rate * Math.pow(1+rate,numberOfPayments))/(Math.pow(1+rate, numberOfPayments)-1);
+    System.out.println("Monthly Payment: " + currency.format(monthlyPayment));
+
+    
+    
+  }
+}
